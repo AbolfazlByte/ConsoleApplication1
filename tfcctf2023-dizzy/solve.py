72e5e238cb1add736a08e12618e846fe48e3bd15
abolfazlbyte <abolfazl.byte@gmail.com>
2023-08-05 18:44:20 +0330

    complete "solve code"

--- tfcctf2023-dizzy/solve.py
+++ tfcctf2023-dizzy/solve.py
@@ -3,10 +3,14 @@ coded_string = "T4 l16 _36 510 _27 s26 _11 320 414 {6 }39 C2 T0 m28 317 y35 d31 
 
 ''' Decode function '''
 def Decoder(code):
+    code_list = code.split()
+    plain_code = 'x' * len(code_list)
+    for c in code_list:
+        index = int(c[1:])
+        value_to_insert = c[0]
+        plain_code = plain_code[:index] + value_to_insert + plain_code[index+1:]
     
-    #TODO
-
-    return
+    return plain_code
 
 
 
